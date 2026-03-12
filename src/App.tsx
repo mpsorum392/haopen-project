d5ec7c8d9b95a7a6aa9016ebb3fb6800fa3553f6
mpsorum392 <mpsorum392@hotmail.com>
2026-03-11 22:08:17 -0500

    updates to l/d and more

--- src/App.tsx
+++ src/App.tsx
@@ -136,10 +136,10 @@ function HomeView({ bluePoints, pinkPoints }: { bluePoints: number, pinkPoints: 
         </div>
 
         <section className="glass-panel p-8 rounded-3xl">
-          <h3 className="text-2xl font-display font-bold mb-6 flex items-center gap-2 tracking-tight">
+          <h3 className="text-3xl font-display font-bold mb-6 flex items-center gap-2 tracking-tight">
             Event Overview
           </h3>
-          <p className="text-md leading-relaxed opacity-60 mb-8">
+          <p className="text-xl leading-relaxed opacity-60 mb-8">
             Welcome to the 7th Annual Hackers & Addicts Open. 12 golfers, 2 teams, 3 days of grueling match play across Iowa's finest courses.
             The Blue Hackers face off against the Pink Addicts in a quest for eternal glory.<br/>
           </p>
@@ -148,7 +148,7 @@ function HomeView({ bluePoints, pinkPoints }: { bluePoints: number, pinkPoints: 
           <h2 className="text-2xl font-display font-bold mb-6 flex items-center gap-2 tracking-tight">
             What's this going to cost you?
           </h2>
-          <p className="text-md leading-relaxed opacity-60 mb-8">
+          <p className="text-lg leading-relaxed opacity-60 mb-8">
               Entry Fees:
               <ul style={{ paddingLeft: '20px', listStyleType: 'disc' }}>
                   <li>$50 to go towards team competition</li>
@@ -177,25 +177,26 @@ function HomeView({ bluePoints, pinkPoints }: { bluePoints: number, pinkPoints: 
           <h2 className="text-2xl font-display font-bold mb-6 flex items-center gap-2 tracking-tight">
             Travel Plans
           </h2>
-          <p className="text-md leading-relaxed opacity-60 mb-8">
-              Wednesday the 24th - Leaving Home
+          <p className="text-lg leading-relaxed opacity-60 mb-8">
+              <b>Wed the 24th - Leaving Home</b>
              <ul style={{ paddingLeft: '20px', listStyleType: 'disc' }}>
               <li>We will leave Wednesday the 24th no later than 5pm in order to arrive to the Riverside Casino by 930p</li>
               <li>Shared rooms for the group with stay and play package</li>
              </ul>
-              Thursday the 25th - Riverside Casino
+              <b>Thur the 25th - Riverside Casino</b>
              <ul style={{ paddingLeft: '20px', listStyleType: 'disc' }}>
               <li>Morning 18 will be our first competition round</li>
               <li>Afternoon 18 for those who desire or casino time or pool/spa visits</li>
               <li>Evening drive into Burlington and arrive to Spirit Hollow GC by 9pm (hour drive)</li>
              </ul>
-              Friday 26th & Saturday 27th - Spirit Hollow
+              <b>Fri 26th & Sat 27th - Spirit Hollow</b>
              <ul style={{ paddingLeft: '20px', listStyleType: 'disc' }}>
               <li>36 holes scheduled both days - morning round is competition round and afternoon at your pleasure</li>
              </ul>
-              Sunday the 28th - Heading Home
+              <b>Sun the 28th - Heading Home</b>
              <ul style={{ paddingLeft: '20px', listStyleType: 'disc' }}>
-              <li>Have a tee time on the books for early Sunday but in years past it is up in the air on if anyone stays and plays or we head home</li>
+              <li>Have a tee time on the books for early Sunday AM</li>
+                 <li>In years past it is up in the air if anyone plays or we head home</li>
              </ul>
           </p>
           </section>
@@ -481,7 +482,7 @@ interface CrewMember {
 const CREW: CrewMember[] = [
   { name: 'Mike Sorum',      nickname: 'Commish',handicap: 7,  age: 45, image: mikePic,   likes: ['Making plans', 'Bossing people around', 'Buying golf gear', 'Angry tirades under his breath'], dislikes: ['People that do not follow his plans', 'Richards', 'Golf without gambling'] },
   { name: 'Kyle Koehler',    nickname: 'Killer', handicap: 16, age: 45, image: kyleKPic,  likes: ['High paying contracts', 'Silencers', 'Long-range rifles', 'John Wick movies'], dislikes: ['Losing to Sorum', 'Jammed up firing pins', 'Antonio Banderas'] },
-  { name: 'Kevin DeMarco',   nickname: 'Outs',   handicap: 14, age: 40, image: kevinPic,  likes: ['Any chance to win on river', 'Building amazing simulators for his friends', 'Counting numbers'], dislikes: ['Breaking 90', 'Swinging less than 120%', 'Low limit BJ tables'] },
+  { name: 'Kevin DeMarco',   nickname: 'Outs',   handicap: 15, age: 40, image: kevinPic,  likes: ['Any chance to win on river', 'Building amazing simulators for his friends', 'Counting numbers'], dislikes: ['Breaking 90', 'Swinging less than 120%', 'Low limit BJ tables'] },
   { name: 'Derek Bernacchi', nickname: 'Juice',  handicap: 27, age: 38, image: derekPic,  likes: ['Jose Canseco', 'Hitting bombs (with his SW)', 'Direct sunlight'], dislikes: ['None', 'Literally nothing'] },
   { name: 'Jeff Fitzke',     nickname: 'Out of Bounds Right!', handicap: 12, age: 50, image: fitzkePic, likes: ['Raw steak', 'Green golf balls', 'The field of tall grass right of the deep rough'], dislikes: ['Being sober', 'Unchopped wood', 'Unopened wine bottles'] },
   { name: 'Rob Fabian',      nickname: 'Drunk Dont Care', handicap: 12, age: 44, image: robPic,    likes: ['Sailor Jerry', 'Banana slices', 'Staircase slides'], dislikes: ['Fun haters', 'Virgin margaritas'] },
